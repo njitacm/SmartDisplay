d946e403b551df2f01fdb06551e566a623c2264b
Grant Butler <butler.grant.j@gmail.com>
2013-02-25 11:05:42 -0500

    Commented out TwitterStream code until that gets re-added.

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 var express = require('express'),
-	sio = require('socket.io'),
-	TwitterStream = require('./TwitterStream').TwitterStream;
+	sio = require('socket.io');
+/* 	TwitterStream = require('./TwitterStream').TwitterStream; */
 
 var app = express.createServer(),
-	io = sio.listen(app),
-	ts = new TwitterStream();
+	io = sio.listen(app);
+/* 	ts = new TwitterStream(); */
 
 app.configure(function() {
 	app.use(express.static(__dirname + '/public'));
 });
 
+/*
 ts.userID = '81022776';
 ts.use(io.sockets);
 ts.connect();
+*/
 
 app.listen(3000);
