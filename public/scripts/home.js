e55c5d0cc92768825f14370754ae9abe0ed4c13e
Grant J. Butler <butler.grant.j@gmail.com>
2013-05-14 19:36:28 -0300

    Update home.js

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -118,7 +118,7 @@ var Home = Page.extend({
 	
 	_updateDisplay: function _updateDisplay() {
 		if(this._nextMeeting == -1) {
-			this._nowHeading.innerText = "Next Spring";
+			this._nowHeading.innerText = "Next Fall";
 			
 			this._nowHeading.style.display = 'block';
 			this._countdownHeading.style.display = 'none';
